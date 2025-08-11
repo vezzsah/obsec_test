75bd27dc3a650fccefba173c3424899a9575f1c6
GomaEba <ignacio.vezzoni@gmail.com>
2025-08-11 14:28:06 +0000

    fixed name issue

--- infra/main.tf
+++ infra/main.tf
@@ -35,7 +35,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     preemptible     = true
     machine_type    = "e2-micro"
     disk_size_gb    = 10
-    service_account = google_service_account.default.email
+    service_account = google_service_account.gke-sa.email
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
